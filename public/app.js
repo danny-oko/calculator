1e4c9f39c5559d30212b70e5e5aae283dc4f89d8
dnii <123633815+danny-oko@users.noreply.github.com>
2025-12-04 13:05:48 +0800

    nice

--- public/app.js
+++ public/app.js
@@ -1,22 +1,44 @@
 const input = document.getElementById("input");
+const funcButtons = [...document.querySelectorAll(".btn-func")];
+const opButtons = [...document.querySelectorAll(".btn-op")];
 const numberButtons = [...document.querySelectorAll(".btn-num")];
-const funcButtons = [...document.querySelectorAll("btn-func")];
-const opButtons = [...document.querySelectorAll("btn-op")];
 
 let firstValue = null;
 let waitingSecondValue = false;
 let operator = null;
 
 numberButtons.forEach((btn) => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener("click", (e) => {
     const value = btn.textContent;
-
     if (input.value === "0") {
-      input.value = value;
+      firstValue = input.value = value;
     } else {
-      input.value += value;
+      firstValue = input.value += value;
     }
+    console.log("first value:", Number(firstValue));
   });
 });
 
+funcButtons.forEach((func) => {
+  func.addEventListener("click", () => {
+    const funcValue = func.textContent;
 
+    if (input.value !== "" && funcValue === "AC") {
+      input.value = "0";
+    }
+    // get last index and remove it from the number
+    if (funcValue === "C") {
+      const modifiedNum = Math.floor(input.value / 10);
+      console.log(modifiedNum);
+      let updateValue = (input.value = modifiedNum);
+      input.textContent = updateValue;
+    }
+    // find remainder
+  });
+});
+
+opButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+     
+  });
+});
