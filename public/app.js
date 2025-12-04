32a993cfad71f0ebadd30906b11972902d10853a
dnii <123633815+danny-oko@users.noreply.github.com>
2025-12-04 09:55:34 +0800

    initial

--- public/app.js
+++ public/app.js
@@ -1,5 +1,22 @@
-const input = document.getElementById("num");
-const functionalBtn = document.getElementById("functionalBtns");
-const numButton = document.getElementById("numButton");
+const input = document.getElementById("input");
+const numberButtons = [...document.querySelectorAll(".btn-num")];
+const funcButtons = [...document.querySelectorAll("btn-func")];
+const opButtons = [...document.querySelectorAll("btn-op")];
 
-input.textContent = 0;
+let firstValue = null;
+let waitingSecondValue = false;
+let operator = null;
+
+numberButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const value = btn.textContent;
+
+    if (input.value === "0") {
+      input.value = value;
+    } else {
+      input.value += value;
+    }
+  });
+});
+
+
