7fee32ab338a9652da01cbf0bb99f7cfbef9db2b
dnii <123633815+danny-oko@users.noreply.github.com>
2025-12-08 12:26:49 +0800

    nothing

--- public/app.js
+++ public/app.js
@@ -7,6 +7,8 @@ let firstValue = null;
 let operator = null;
 let waitingSecond = false;
 
+// console.log(funcButtons);
+
 numberButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
     const value = btn.textContent;
@@ -16,17 +18,64 @@ numberButtons.forEach((btn) => {
       waitingSecond = false;
       return;
     }
-  });
 
-  if (input.value === "0" || input.value === "Error") {
-    input.value = value;
-  } else {
-    input.value += value;
-  }
-});
-
-funcButtons.forEach((func) => {
-  func.addEventListener("click", () => {
-    
+    if (input.value === "0" || input.value === "Error") {
+      input.value = value;
+    } else {
+      input.value += value;
+    }
   });
 });
+
+funcButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const value = btn.textContent;
+    if (value === "AC") {
+      input.value = 0;
+    }
+    if (value === "C") {
+      let number = input.value;
+      const newNumber = Math.floor(number / 10);
+      input.value = newNumber;
+    }
+    if (value === "+/−") {
+      let number = input.value;
+      const newNum = number * -1;
+      input.value = newNum;
+    }
+  });
+});
+
+opButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const value = btn.textContent;
+
+    if (value === "%") {
+      console.log("helo");
+    }
+  });
+});
+
+// // function operation(a, b, op) {
+
+// // }
+
+// const ans = [];
+// var convertTemperature = function (celsius) {
+//   ans.push((kelvin = celsius + 273.15));
+//   ans.push((faranheit = celsius * 1.8 + 32.0));
+
+//   return ans;
+// };
+
+// console.log(convertTemperature(36.5));
+
+// let commonFactors = function (a, b) {
+//   let arr = [];
+//   for (let i = 1; i < a && i < b; i++) {
+//     if (a % 2 === 0 && b % 2 === 0) {
+//       console.log(i);
+//     }
+//   }
+// };
+// console.log(commonFactors(12, 8));
